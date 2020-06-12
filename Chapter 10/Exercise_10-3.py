6995f2474bb01b99b5a08853173a7d6ef1c4d815
André Fettouhi <A.Fettouhi@gmail.com>
2020-06-12 09:50:41 +0200

    Added code examples and exercise solutions from Chapter 10!

---
+++ Chapter 10/Exercise_10-3.py
@@ -0,0 +1,6 @@
+def middle(l):
+    return l[1:len(l) - 1]
+
+
+t = [1, 2, 3, 4]
+print(middle(t))
---
+++ Chapter 10/Exercise_10-7.py
@@ -0,0 +1,10 @@
+def has_duplicate(l):
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[i] == l[j]:
+                return True
+    return False
+
+
+print(has_duplicate([1, 2, 3, 4, 5]))
+print(has_duplicate([1, 2, 3, 4, 5, 5]))

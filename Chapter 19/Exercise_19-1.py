54029a185858566d2d0eaa83a9bca33ea0ed3329
André Fettouhi <A.Fettouhi@gmail.com>
2020-06-18 05:06:34 +0200

    Added code examples and solutions to exercises for Chapter 19!

---
+++ Chapter 19/Exercise_19-1.py
@@ -0,0 +1,45 @@
+def binomial_coeff(n, k):
+    """
+    Compute the binomial coefficient "n choose k".
+
+    n: number of trials
+    k: number of successes
+
+    returns: int
+    """
+    if k == 0:
+        return 1
+    if n == 0:
+        return 0
+
+    res = binomial_coeff(n-1, k) + binomial_coeff(n-1, k-1)
+    return res
+
+
+def b(n, k):
+    memo = {}
+    if (n, k) in memo:
+        return memo[(n, k)]
+    if k == 0:
+        return 1
+    if n == 0:
+        return 0
+
+    r = b(n - 1, k) + b(n - 1, k - 1)
+    memo[(n, k)] = r
+    return r
+
+
+if __name__ == '__main__':
+    a = binomial_coeff(3, 4)
+    # b(3,4) = b(2, 4) + b(2,3)
+    # b(2,4) = b(1,4) + b(1,3)
+    # b(1,4) = b(0,4) + b(0,3) = 0 + 0 = 0
+    # b(1,3) = b(0,3) + b(0,2) = 0 + 0 = 0
+    # b(2,3) = b(1,3) + b(1,2)
+    # b(1,2) = b(0,2) + b(0,1) = 0 + 0 = 0
+    print(a)
+    print(b(3, 4))
+    c = binomial_coeff(10, 4)
+    print(c)
+    print(b(10, 4))
